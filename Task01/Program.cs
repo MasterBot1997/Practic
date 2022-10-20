50a53e4c88cc44cb4d058d84ee35e853a835aab9
Ivan <bel3187227@yandex.ru>
2022-10-20 04:49:51 +0300

    add Task01

--- Task01/Program.cs
+++ Task01/Program.cs
@@ -1,2 +1,13 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Напиште программу которая будет определять является ли 
+// первое число квадратом второго числа
+Console.WriteLine("Введите два числа: ");
+int number1 = Convert.ToInt32(Console.ReadLine());
+int number2 = Convert.ToInt32(Console.ReadLine());
+if(number1 == number2 * number2)
+{
+    Console.WriteLine("Да");
+}
+else
+{
+    Console.WriteLine("Нет");
+}
