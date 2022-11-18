cdf62966161015df90d9e02c0b94176675ea0c80
Ivan <bel3187227@yandex.ru>
2022-11-18 18:11:04 +0300

    Прорешали задачи 4 семинара

---
+++ Task26/Program.cs
@@ -0,0 +1,26 @@
+﻿// Задача 26: Напишите программу, 
+//1. которая принимает на вход число и 
+//2. выдаёт количество цифр в числе.
+// 456 -> 3
+// 78 -> 2
+// 89126 -> 5
+
+Console.Write("Введите число: ");
+int num = Convert.ToInt32(Console.ReadLine());
+
+int SumDigit(int number)
+{
+int k = 0;
+number = Math.Abs(number);
+while (number > 0)
+{
+    number = number / 10;
+    k = k+1;
+}
+return k;
+}
+
+int result = SumDigit(num);
+if (num == 0) Console.WriteLine($"Число {num} = 1");
+else Console.WriteLine($"Число {num} состоит из {result} цифр");
+
---
+++ Task28/Program.cs
@@ -0,0 +1,21 @@
+﻿// Задача 28: Напишите программу, которая
+// принимает на вход число N и выдаёт
+// произведение чисел от 1 до N.
+// 4 -> 24
+// 5 -> 120
+
+Console.Write("Введите чсло N: ");
+int n = Convert.ToInt32(Console.ReadLine());
+
+int N(int num)
+{
+    int fact = 1;
+    for (int i = 1; i <= num; i++)
+    {
+        fact *= i;
+    }
+    return fact;
+}
+
+int result = N(n);
+Console.WriteLine(result);
