c5c23987481bdf08550bef40a1ed7fa7d892c793
Ivan <bel3187227@yandex.ru>
2022-11-21 20:06:27 +0300

    Прорешали задачи 5 семинара

---
+++ Task31/Program.cs
@@ -0,0 +1,49 @@
+﻿// // Задача 31: 
+// 1. Задайте массив из 12 элементов, 
+// 2. заполненный случайными числами из промежутка [-9, 9]. 
+// 3. Найдите сумму отрицательных 
+// 4. и положительных элементов массива.
+// // Например, в массиве [3,9,-8,1,0,-7,2,-1,8,-3,-1,6] сумма
+// // положительных чисел равна 29, сумма отрицательных равна
+// // -20
+
+int[] CreateArrayRndInt(int size, int min, int max)
+{
+    int[] array = new int[size];
+    Random rnd = new Random();
+    for (int i = 0; i < size; i++)
+    {
+        array[i] = rnd.Next(min, max + 1);
+    }
+    return array;
+}
+
+void PrintArray(int[] array)
+{
+    Console.Write("[");
+    for (int i = 0; i < array.Length; i++)
+    {
+        if (i < array.Length - 1) Console.Write($"{array[i]},");
+        else Console.WriteLine($"{array[i]}");
+    }
+    Console.Write("]");
+}
+
+int[] GetSumPositiveNegativeElem(int[] array)
+{
+    int sumPositiv = 0;
+    int sumNegativ = 0;
+    for (int i = 0; i < array.Length; i++)
+    {
+        if (array[i] > 0) sumPositiv += array[i];
+        else sumNegativ += array[i];
+    }
+
+    return new int[]{sumPositiv, sumNegativ};
+}
+
+int[] arr = CreateArrayRndInt(12, -9, 9);
+PrintArray(arr);
+int[] getSumPositiveNegativeElem = GetSumPositiveNegativeElem(arr);
+Console.WriteLine($"Сумма положительных элементов = {getSumPositiveNegativeElem[0]}");
+Console.WriteLine($"Сумма отрицательных элементов = {getSumPositiveNegativeElem[1]}");
---
+++ Task32/Program.cs
@@ -0,0 +1,40 @@
+﻿// Задача 32: Напишите программу замена элементов
+// массива: положительные элементы замените на
+// соответствующие отрицательные, и наоборот.
+// [-4, -8, 8, 2] -> [4, 8, -8, -2]
+
+
+int[] CreateArrayRndInt(int size, int min, int max)
+{
+    int[] array = new int[size];
+    Random rnd = new Random();
+    for (int i = 0; i < size; i++)
+    {
+        array[i] = rnd.Next(min, max + 1);
+    }
+    return array;
+}
+
+void InversionArray(int[] arr)
+{
+    for (int i = 0; i < arr.Length; i++)
+    {
+        arr[i] *= -1;
+    }
+}
+
+void PrintArray(int[] array)
+{
+    Console.Write("[");
+    for (int i = 0; i < array.Length; i++)
+    {
+        if (i < array.Length - 1) Console.Write($"{array[i]},");
+        else Console.Write($"{array[i]}");
+    }
+    Console.WriteLine("]");
+}
+
+int[] array = CreateArrayRndInt(4, -9, 9);
+PrintArray(array);
+InversionArray(array);
+PrintArray(array);
