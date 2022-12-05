0cb5d0c1ce423bd613a4334674b3a07b860d285e
Ivan <bel3187227@yandex.ru>
2022-12-05 16:40:30 +0300

    прорешали 9 семинар

---
+++ Task67/Program.cs
@@ -0,0 +1,16 @@
+﻿// Задача 67: Напишите программу, которая будет принимать на вход число и
+// возвращать сумму его цифр.
+// 453 -> 12
+// 45 -> 9
+
+Console.WriteLine("введите число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+
+int Factorial(int num)
+{
+    if (num == 0) return num;
+    return num % 10 + Factorial(num / 10);
+}
+
+int sumFac = Factorial(number);
+Console.WriteLine($"Сумма числа {number} -> {sumFac} ");
